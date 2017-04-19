040438081d7f6f2506ad61d0325380c7d09b1dc9
dandevelopment <daniel.cambourelis@lca.edu>
2017-04-19 13:03:59 -0400

    Update tools.js

--- tools.js
+++ tools.js
@@ -63,7 +63,18 @@ return document.getElementById(place)
 if(getCollegeOptions() == ''){
 addCollegeOption('none')
 }
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
 
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
 function notify(message){
     var newE = document.createElement('div');
     newE.style.width = '30%';
