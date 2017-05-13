98fa06ba346ec9b7d1b841131d51282b84624496
dandevelopment <daniel.cambourelis@lca.edu>
2017-05-13 12:56:19 -0400

    Update tools.js

--- tools.js
+++ tools.js
@@ -84,21 +84,22 @@ function addDownloadRecord(filename, text, type){
   return ''
 }
   function readSingleFile(evt) {
-    alert('worked')
+ //   alert('worked')
     //Retrieve the first (and only!) File from the FileList object
     var f = evt.target.files[0];
 
     if (f) {
       var r = new FileReader();
       r.onload = function (e) {
-          alert('really worked.')
+      //    alert('really worked.')
 	      var contents = e.target.result;
-        document.getElementById('todisplay').innerHTML =  "Got the file.n"
+       /* "Got the file.n"
               +"name: " + f.name + "n"
               +"type: " + f.type + "n"
               +"size: " + f.size + " bytesn"
               + "starts with: " + contents//.substr(1, contents.indexOf("n"))
-        
+        */
+	      return contents;
       }
       r.readAsText(f);
     } else {
