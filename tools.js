3a2675c242eb6dc34561e10e6ba3087a536cf943
dandevelopment <daniel.cambourelis@lca.edu>
2017-05-24 16:03:08 -0400

    Update tools.js

--- tools.js
+++ tools.js
@@ -574,10 +574,12 @@ function getCookies2() {
 		console.log('searching values ' + i + 'th time')
 		i2 = 0;
 	       while(values.length > i2){
-		       console.log('hmmmm... does ' + values[i2].split('#$')[3] + ' = ' + Number(organizedValues.length + 1))
-		   if(Number(values[i2].split('#$')[3]) == organizedValues.length + 1){
-		     organizedValues.push(values[i2])
-		   }
+		       if(values[i2].split('#$')[1] != 'sync'){
+		           console.log('hmmmm... does ' + values[i2].split('#$')[3] + ' = ' + Number(organizedValues.length + 1))
+	         	   if(Number(values[i2].split('#$')[3]) == organizedValues.length + 1){
+	          	     organizedValues.push(values[i2])
+		           }
+		       }
                i2++
 	       }
 		i++
