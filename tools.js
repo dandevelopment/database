58cbd8f3bcf014f5e59381142c6acb1efeb11e76
dandevelopment <daniel.cambourelis@lca.edu>
2017-07-12 12:29:33 -0400

    Update tools.js

--- tools.js
+++ tools.js
@@ -51,7 +51,7 @@ setTimeout(function(){
 //setLitteralItem('undefined','')
 resetAchievments()
 function putInConsole(data){
-setLitteralItem('consoleToken', getLitteralItem('consoleToken') + '<br>' + data)
+setLitteralItem('consoleToken', data + '<br>' + getLitteralItem('consoleToken'))
 }
 
 var searchedForCookies = false;
