bbafe9db890008f51cc767c9f4879ba5fbe2dc46
dandevelopment <daniel.cambourelis@lca.edu>
2017-07-13 11:44:44 -0400

    Update tools.js

--- tools.js
+++ tools.js
@@ -274,6 +274,7 @@ var checkForAchievment = setInterval(function () {
         }
         // set as the difference between what is already set as the hours for law enforcement training and the tally of what needs to be added
 	totalCompletedForUser = totalCompletedForUser - getAchievmentCompletedHours(i, 'law enforcement training')
+	    logit('the difference is ' + totalCompletedForUser)
         //if there is no difference
         if (totalCompletedForUser != 0 && totalCompletedForUser != NaN) {
             logit('adding... ' + totalCompletedForUser)
